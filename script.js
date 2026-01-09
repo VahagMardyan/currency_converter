40eeb6eb44f3ae01097bc70d1663edbb18e9da6d
VahagMardyan <mardyan2003@gmail.com>
2026-01-09 16:43:17 +0400

    Modified UI. Added swap function and flags for each country.

--- script.js
+++ script.js
@@ -11,6 +11,33 @@ const btn_convert = document.querySelector('#convert');
 const btn_convert_by_real_time = document.querySelector('#convertByRealTime');
 const btn_reset = document.querySelector('#reset');
 
+const btn_swap = document.getElementById('swap-btn');
+const source_flag = document.getElementById('source-flag');
+const target_flag = document.getElementById('target-flag');
+
+function updateFlag(selectElement, flagElement) {
+    let countryCode = selectElement.value.slice(0,2);
+    if(selectElement.value === "EUR") {
+        flagElement.src = "./logo/eu_flag.png";
+    } else {
+        flagElement.src = `https://flagsapi.com/${countryCode}/flat/64.png`;
+    }
+    flagElement.alt = countryCode;
+    const global_flag = "./logo/global_flag.png";
+    flagElement.onerror = () => {
+        flagElement.src = global_flag;
+        flagElement.onerror = null;
+    }
+}
+
+select_source.addEventListener('change', () => {
+    updateFlag(select_source, source_flag); 
+});
+
+select_target.addEventListener('change', () => {
+    updateFlag(select_target, target_flag);
+});
+
 const LOCAL_JSON = "./json_files/data.json";
 const API_URL = "https://api.currencyapi.com/v3/latest?apikey=KL7RHTiYo19Y1yaIozLDbXLfdL3VdjvtxbOXL6SV";
 
@@ -26,17 +53,34 @@ function renderSourceOptions() {
     const searchTerm = search_source.value.toLowerCase();
     select_source.innerHTML = '';
 
+    if(searchTerm === "") {
+        sourceOptions.forEach(option => select_source.append(option));
+        select_source.selectedIndex = 0;
+        updateFlag(select_source, source_flag);
+        return;
+    }
+
     sourceOptions.forEach((option) => {
         const optionText = option.innerText.toLowerCase();
         if (optionText.includes(searchTerm) || searchTerm === '') {
             select_source.append(option);
         }
     });
+    if(select_source.options.length > 0) {
+        updateFlag(select_source, source_flag);
+    }
 }
 
 function renderTargetOptions() {
     const searchTerm = search_target.value.toLowerCase();
     select_target.innerHTML = '';
+
+    if(searchTerm === "") {
+        targetOptions.forEach(option => select_target.append(option));
+        select_target.selectedIndex = 0;
+        updateFlag(select_target, target_flag);
+        return;
+    }
 
     targetOptions.forEach((option) => {
         const optionText = option.innerText.toLowerCase();
@@ -44,7 +88,13 @@ function renderTargetOptions() {
             select_target.append(option);
         }
     });
+    if(select_target.options.length > 0) {
+        updateFlag(select_target, target_flag);
+    }
 }
+
+search_source.addEventListener('input', renderSourceOptions);
+search_target.addEventListener('input', renderTargetOptions);
 
 // IIFE function
 (async function() {
@@ -67,6 +117,26 @@ btn_convert_by_real_time.addEventListener('click', (event) => {
 
 btn_reset.addEventListener('click', () => {
     location.reload();
+});
+
+btn_swap.addEventListener('click', ()=> {
+    const currentSource = select_source.value;
+    const currentTarget = select_target.value;
+    
+    search_source.value = "";
+    search_target.value = "";
+
+    select_source.innerHTML = "";
+    sourceOptions.forEach(option => select_source.append(option));
+
+    select_target.innerHTML = "";
+    targetOptions.forEach(option => select_target.append(option));
+
+    select_source.value = currentTarget;
+    select_target.value = currentSource;
+
+    updateFlag(select_source, source_flag);
+    updateFlag(select_target, target_flag);
 });
 
 document.addEventListener('keypress', (event) => {
